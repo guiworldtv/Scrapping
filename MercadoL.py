d3ff83d7297ba165573f7389f989dadda0d6650f
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-31 17:08:21 -0300

    Update MercadoL.py

--- MercadoL.py
+++ MercadoL.py
@@ -1,51 +1,56 @@
-from http.client import PRECONDITION_FAILED
-from urllib import response
+from os import path
+from flask import Flask, render_template
 from bs4 import BeautifulSoup
+import shutil
 import requests
-'''
-prettify() == vai indentar o código para uma melhor anásile
-{nome}={site}.find('div' , attrs={'class' : 'class_completa'})  == vai pegar
-os codigos contidos dentro dessa div
-print({nome}.prettify()) --> vai mandar o arquivo totalmente identado...
 
-Proximo documento utiliza selenium
+# Urls
+url_base = "https://apod.nasa.gov/apod/"
+src = requests.get(url_base + "astropix.html").text
 
-'''
+# Soup
+soup = BeautifulSoup(src, 'lxml')
+center = soup.select('body > center')[2]
+yesterday = center.find('hr').findNext('a').attrs['href']
+img = soup.select('body > center > p')[1].find('a')
 
-valor = 1;
+url_ext = img.attrs['href']
+full_url = url_base + url_ext
 
-pesquisa = ''
+# Today's picture
+r = requests.get(full_url, stream=True)
+if r.status_code == 200:
+    file = open("./static/downloads/potd_nasa.jpg", "wb")
+    file.write(r.content)
+    file.close()
 
-def scrapping():
-    contador =1
-    url_base = f'https://lista.mercadolivre.com.br/{pesquisa}#D[A:{pesquisa}]'
-    response = requests.get(url_base)
-    site =  BeautifulSoup(response.text, 'html.parser') # content!=text
-    produtos = site.findAll('div',attrs={'class': 'andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default'} )
-    for produto in produtos:
-        titulo = produto.find('h2', attrs={'class' : 'ui-search-item__title'})
-        link_produto = produto.find('a' , attrs={'class': 'ui-search-item__group__element ui-search-link'})
-        reais = produto.find('span', attrs={'class':'price-tag-fraction'})
-        centavos = produto.find('span', attrs={'class': 'price-tag-cents'})
-        #print(produto.prettify())
-        #'''
-        print(f"PRODUTO {contador}-------------------------------")
-        contador = contador +1
-        print(f'PRODUTO: {titulo.text}')
-        if centavos:
-            print(f'PREÇO: R${reais.text},{centavos.text}')
-        else:
-            print(f'PREÇO: R${reais.text}')
-        print('LINK PARA O PRODUTO: ', link_produto['href'])
-        print("------------------------------------------\n\n")
-        #'''
-    
-    
+# Last 5 days
+for i in range(5):
+    prev_page = url_base + yesterday
+    src = requests.get(prev_page).text
+    soup = BeautifulSoup(src, 'lxml')
 
-pesquisa = str(input("Qual produto deseja pesquisa no Mercado Livre: "))
-scrapping()
+    center = soup.select('body > center')[2]
+    yesterday = center.find('hr').findNext('a').attrs['href']
+    img = soup.select('body > center > p')[1].find('a')
 
-    
+    url_ext = img.attrs['href']
+    full_url = url_base + url_ext
+
+    img_path = "./static/downloads/yesterday_potd_" + str(i+1) + ".jpg"
+    r = requests.get(full_url, stream=True)
+    if r.status_code == 200:
+        file = open(img_path, "wb")
+        file.write(r.content)
+        file.close()
 
 
+# Flask 
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    head = soup.select('body > center')[0].find('h1').text
+    return render_template('index.html', **locals())
+
+app.run(debug=True)
